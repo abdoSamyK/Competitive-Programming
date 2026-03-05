2ef0bdae5d91b06529d80c30a37e7dfad6e716a8
Abdelrhman Samy <abdelrahmankrelah@gmail.com>
2026-03-05 06:46:19 +0200

    Update ncr with mod.cpp

--- math/combinatorics/ncr/ncr with mod.cpp
+++ math/combinatorics/ncr/ncr with mod.cpp
@@ -1,6 +1,6 @@
 const int N = 2e5 + 2;
 const ll mod = 1e9 + 7;
-ll fac[N + 5], inv[N + 5];
+ll fac[N + 5], invfac[N + 5];
 
 ll fastpow(ll a, ll b) {
   ll res = 1;
@@ -16,9 +16,9 @@ void factorial() {
   for (int i = 1; i < N; i++) { fac[i] = fac[i - 1] * i % mod; }
 }
 void inversefact() {
-  inv[N - 1] = fastpow(fac[N - 1], mod - 2);
+  invfac[N - 1] = fastpow(fac[N - 1], mod - 2);
   for (int i = N - 1; i >= 1; i--) {
-    inv[i - 1] = inv[i] * i % mod;
+    invfac[i - 1] = invfac[i] * i % mod;
   }
 }
 void initncr() {
@@ -28,5 +28,5 @@ void initncr() {
 }
 ll nCr(int n, int r) {
   if (r > n || r < 0) return 0;
-  return fac[n] * inv[r] % mod * inv[n - r] % mod;
+  return fac[n] * invfac[r] % mod * invfac[n - r] % mod;
 }
