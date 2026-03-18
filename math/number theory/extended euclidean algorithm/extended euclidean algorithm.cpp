b69af1c345c4b3de206186da8b9153d6ebc7bed9
AbdoSamyK <abdelrahmankrelah@gmail.com>
2026-03-18 15:00:24 +0200

    Update extended euclidean algorithm.cpp

--- math/number theory/extended euclidean algorithm/extended euclidean algorithm.cpp
+++ math/number theory/extended euclidean algorithm/extended euclidean algorithm.cpp
@@ -15,7 +15,7 @@ int eeaGCD(int a, int b, int& x, int& y) {
     return a;
   }
   int x1, y1;
-  int d = gcd(b, a % b, x1, y1);
+  int d = eeaGCD(b, a % b, x1, y1);
   x = y1;
   y = x1 - y1 * (a / b);
   return d;
