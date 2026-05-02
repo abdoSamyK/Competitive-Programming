8b77e61500d267cdc0e4a3a41bd4cc0d7fd9fb9c
AbdoSamyK <abdelrahmankrelah@gmail.com>
2026-05-03 02:08:15 +0300

    readd sparse table implementation

--- DataStructures/sparse table/sparse_table.cpp
+++ DataStructures/sparse table/sparse_table.cpp
@@ -6,24 +6,29 @@ struct sparseTable {
     return min(a, b);
   }
 
-  sparseTable(vector<int> &v) {
+  sparseTable(const vector<int> &v) {
     int n = (int)v.size();
     lg.assign(n + 1, 0);
-    for (int i = 2;i <= n;++i) {
-      lg[i] = 1 + lg[i/2];
+    for (int i = 2; i <= n; ++i)
+      lg[i] = 1 + lg[i / 2];
+
+    int mxlg = lg[n];
+    spt.assign(mxlg + 1, vector<int>(n));
+
+    for (int j = 0; j < n; ++j) {
+      spt[0][j] = v[j];
     }
-    spt.assign(lg[n] + 1, vector<int>(n));
-    spt[0] = v;
-    for (int i = 1; i <= lg[n]; ++i) {
-      int l = (1 << i);
-      for (int j = 0; j + l <= n; ++j) {
-        spt[i][j] = merge(spt[i - 1][j], spt[i - 1][j + (l >> 1)]);
+
+    for (int i = 1; i <= mxlg; ++i) {
+      for (int j = 0; j + (1 << i) <= n; ++j) {
+        spt[i][j] = merge(spt[i - 1][j], spt[i - 1][j + (1 << (i - 1))]);
       }
     }
   }
-  int get(int l, int r) {
+
+  int get(int l, int r) { // 0-based
     int len = r - l + 1;
-    int j = lg[len];
-    return merge(spt[j][l], spt[j][r - (1 << j) + 1]);
+    int i = lg[len];
+    return merge(spt[i][l], spt[i][r - (1 << i) + 1]);
   }
 };
